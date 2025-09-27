ab14f4907ad08dc7b0df202096cf5c9b52bd96d8
Wyatt <43044773+WyattLiang@users.noreply.github.com>
2025-09-27 15:47:36 -0400

    completed registration and registration confirmation page.

---
+++ src/main/java/com/bny/pages/flightreservation/RegistrationPage.java
@@ -0,0 +1,70 @@
+package com.bny.pages.flightreservation;
+
+import com.bny.pages.AbstractPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class RegistrationPage extends AbstractPage {
+
+
+    @FindBy(id = "firstName")
+    private WebElement firstNameInput;
+
+    @FindBy(id = "lastName")
+    private WebElement lastNameInput;
+
+    @FindBy(id = "email")
+    private WebElement emailInput;
+
+    @FindBy(id = "password")
+    private WebElement passwordInput;
+
+    @FindBy(id = "street")
+    private WebElement streetInput;
+
+    @FindBy(id = "city")
+    private WebElement cityInput;
+
+    @FindBy(id = "zipcode")
+    private WebElement zipcodeInput;
+
+    @FindBy(id = "register_btn")
+    private WebElement registerButton;
+
+    public RegistrationPage(WebDriver driver){
+        super(driver);
+    }
+
+    @Override
+    public boolean isAt() {
+        this.wait.until(ExpectedConditions.visibilityOf(this.firstNameInput));
+        return this.firstNameInput.isDisplayed();
+    }
+
+    public void goTo(String url){
+        this.driver.get(url);
+    }
+
+    public void enterUserDetail(String firstName, String lastName){
+        this.firstNameInput.sendKeys(firstName);
+        this.lastNameInput.sendKeys(lastName);
+    }
+
+    public void enterUserCredential(String email, String password){
+        this.emailInput.sendKeys(email);
+        this.passwordInput.sendKeys(password);
+    }
+
+    public void enterAddress(String street, String city, String zip){
+        this.streetInput.sendKeys(street);
+        this.cityInput.sendKeys(city);
+        this.zipcodeInput.sendKeys(zip);
+    }
+
+    public void register(){
+        this.registerButton.click();
+    }
+}
